8cb6c1d1dbff98da503d673f3086606b848c4468
Integer Limit <103940576+IntegerLimit@users.noreply.github.com>
2025-09-17 08:13:04 +1000

    Fix Over-Caching of LSP Hints

--- src/main/java/com/intlimit/grsplugin/features/TextureHintsProvider.java
+++ src/main/java/com/intlimit/grsplugin/features/TextureHintsProvider.java
@@ -162,8 +162,9 @@ public class TextureHintsProvider extends AbstractLSPInlayHintsProvider {
                 .getServerStatus("groovyscript");
 
         if (status != ServerStatus.started) {
-            // Not initialized yet
+            // Not initialized yet; reset prev results
             log.error("Inlay Hint request cancelling, GrS LSP server not available.");
+            previousResults = CompletableFuture.completedFuture(Collections.emptyList());
             return;
         }
 
